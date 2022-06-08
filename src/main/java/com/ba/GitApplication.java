9a08272c9522f85f7c5d6f91e9f0b63355a60679
BarryAllen <63345686+1249683134@users.noreply.github.com>
2022-06-08 16:18:37 +0800

    hahaha

--- src/main/java/com/ba/GitApplication.java
+++ src/main/java/com/ba/GitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitApplication {
     //master
     //hot-fix
+    //git-test
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class, args);
     }
